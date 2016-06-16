09fd244d16fff448af4bf53292555f7067b90b00
Martin FV <martinv@foliovision.com>
2016-06-16 14:34:08 +0200

    Actually, let's keep checking XML-RPC Pingbacks.

--- businesspress.php
+++ businesspress.php
@@ -133,7 +133,7 @@ class BusinessPress {
     
     add_filter( 'wp_login_failed', array( $this, 'fail2ban_login' ) );
     add_filter( 'xmlrpc_login_error', array( $this, 'fail2ban_xmlrpc' ) );
-    //add_filter( 'xmlrpc_pingback_error', array( $this, 'fail2ban_xmlrpc_ping' ), 5 );
+    add_filter( 'xmlrpc_pingback_error', array( $this, 'fail2ban_xmlrpc_ping' ), 5 );
   }
   
   
@@ -611,10 +611,10 @@ class BusinessPress {
   
   
   function fail2ban_xmlrpc_ping( $ixr_error ) {
-    if( $ixr_error->code === 48 || $ixr_error->code === 33 ) return $ixr_error;
+    if( $ixr_error->code === 48 ) return $ixr_error;
     
     $this->fail2ban_openlog();
-    syslog( LOG_INFO,'BusinessPress fail2ban login error - XML-RPC Pingback error '.$ixr_error->code.' generated from '.$this->get_remote_addr() );
+    syslog( LOG_INFO,'BusinessPress fail2ban pingback error - XML-RPC Pingback error '.$ixr_error->code.' generated from '.$this->get_remote_addr() );
   }
   
   
