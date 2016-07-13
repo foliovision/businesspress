8adac59916f8676b30998cdae7064a53d0b0becd
Martin FV <martinv@foliovision.com>
2016-07-13 12:11:23 +0200

    Trapping all the notices.

--- businesspress-notices.class.php
+++ businesspress-notices.class.php
@@ -5,17 +5,30 @@ class BusinessPress_Notices {
   var $iNoticesAvoided = 0;
   
   public function __construct() {
-    add_action( 'admin_notices', array( $this, 'trap'), 0 );
-    add_action( 'network_admin_notices', array( $this, 'store'), 999999 );
-    
-    add_action( 'admin_notices', array( $this, 'remove'), 0 );
-    add_action( 'admin_footer', array( $this, 'show_count'), 0 );
-    
-    add_action( 'admin_init', array( $this, 'remove_gravityforms'), 0 );
+    if( isset($_GET['martinv']) ) {      
+      add_action( 'admin_notices', array( $this, 'trap'), 0 );
+      add_action( 'admin_notices', array( $this, 'store'), 999999 );      
+      
+    } else {
+      add_action( 'admin_notices', array( $this, 'remove'), 0 );
+      add_action( 'admin_footer', array( $this, 'show_count'), 0 );
+      
+      add_action( 'admin_init', array( $this, 'remove_gravityforms'), 0 );      
+      
+    }
     
     add_action( is_multisite() ? 'network_admin_menu' : 'admin_menu',  array( $this, 'menu' ) );
   }
   
+  
+  
+  
+  function outerHTML($e) {
+    $doc = new DOMDocument();
+    $doc->appendChild($doc->importNode($e, true));
+    return $doc->saveHTML();
+  }
+
   
   
   
@@ -92,19 +105,75 @@ class BusinessPress_Notices {
   function screen() {
     echo "<p>BusinessPress works hard to make sure all the annoying plugin notices show up on this screen only and don't pollute your whole WP Admin Dashboard.</p>";
     
-    do_action('businesspress_admin_notices');
+    if( isset($_GET['martinv']) ) {
+      $aStored = get_option( 'businesspress_notices', array() );      
+      foreach( $aStored AS $aNotice ) {
+        echo "<h5>".date('Y-m-d h:m:s',$aNotice['time'])."</h5>".$aNotice['html'];
+      }
+    } else {   
     
-    if( method_exists('GFForms','dashboard_update_message') ) {
-      remove_filter( 'pre_option_gf_dismissed_upgrades', array( $this, 'remove_gravityforms_action' ) );
-      GFForms::dashboard_update_message();
+      do_action('businesspress_admin_notices');
+      
+      if( method_exists('GFForms','dashboard_update_message') ) {
+        remove_filter( 'pre_option_gf_dismissed_upgrades', array( $this, 'remove_gravityforms_action' ) );
+        GFForms::dashboard_update_message();
+      }
+    
     }
+
+    
   }  
   
   
   
   
   function store() {
-    echo ob_get_clean();
+    $junk = ob_get_clean();
+    $dom = new DOMDocument();
+    $dom->loadHTML( $junk );
+    
+    $aMatches = array();
+    foreach( $dom->getElementsByTagName('div') as $objDiv ) {
+      if( !$objDiv->hasAttribute('class')) {
+        continue;
+      }
+  
+      $aClass = explode(' ', $objDiv->getAttribute('class'));  
+      if( in_array('updated', $aClass) ) {
+        $aMatches[] = $this->outerHTML($objDiv);
+      }
+      if( in_array('update-nag', $aClass) ) {
+        $aMatches[] = $this->outerHTML($objDiv);
+      }
+  
+    }
+    
+    $aStored = get_option( 'businesspress_notices', array() );
+    $aNew = $aStored;
+    foreach( $aMatches AS $sNotice ) {
+      
+      $check_one = preg_replace( '~[^a-z0-9]~', '', strip_tags($sNotice) );
+      
+      $bSkip = false;
+      foreach( $aStored AS $sStored ) {
+        $check_two = preg_replace( '~[^a-z0-9]~', '', strip_tags($sStored['html']) );
+        if( $check_one == $check_two ) {
+          $bSkip = true;
+          break;
+        }
+      }
+      
+      if( !$bSkip ) {
+        $aNew[] = array( 'time' => time(), 'html' => $sNotice );
+      }
+      
+    }
+    
+    update_option( 'businesspress_notices', $aNew );
+    
+    //$html->find('div.updated');
+    //$html->find('div.notice');
+
   }
   
 
