c8ba33d45fe236249f5aac679b79f1bd85d0d9d8
Martin FV <martinv@foliovision.com>
2017-02-10 16:17:11 +0100

    Fix for subscriber detection.

--- businesspress.php
+++ businesspress.php
@@ -215,7 +215,11 @@ class BusinessPress {
     
     if( !empty($this->aOptions['wp_admin_bar_subscribers']) && $this->aOptions['wp_admin_bar_subscribers'] && get_current_user_id() > 0 ) {
       $objUser = get_userdata( get_current_user_id() );
-      if( $objUser && isset($objUser->roles) && count($objUser->roles) == 2 && ( $objUser->roles[0] == 'subscriber' && $objUser->roles[1] == 'bbp_participant' || $objUser->roles[0] == 'bbp_participant' && $objUser->roles[1] == 'subscriber' ) ) {  //  this is silly, but we can't rely on !current_user_can() with edit_posts or delete_posts to detect Subscribers because of bbPress
+      
+      if( $objUser && isset($objUser->roles) && (
+                                                 count($objUser->roles) == 2 && ( $objUser->roles[0] == 'subscriber' && $objUser->roles[1] == 'bbp_participant' || $objUser->roles[0] == 'bbp_participant' && $objUser->roles[1] == 'subscriber' ) ||
+                                                 count($objUser->roles) > 0 && $objUser->roles[0] == 'subscriber'
+                                                ) ) {  //  this is silly, but we can't rely on !current_user_can() with edit_posts or delete_posts to detect Subscribers because of bbPress
         add_filter('show_admin_bar', '__return_false');
         add_action( 'admin_init', array( $this, 'subscriber__dashboard_redirect' ) );
         add_action( 'admin_head', array( $this, 'subscriber__hide_menus' ) );
