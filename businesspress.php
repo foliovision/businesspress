0749a6ae750b62f8b67d5ff48bfbb2c219e240d1
Martin FV <martinv@foliovision.com>
2017-04-05 11:57:17 +0200

    Version number!

--- businesspress.php
+++ businesspress.php
@@ -3,7 +3,7 @@
 Plugin Name: BusinessPress
 Plugin URI: http://www.foliovision.com
 Description: This plugin secures your site
-Version: 0.7.1
+Version: 0.7.2
 Author: Foliovision
 Author URI: http://foliovision.com
 */
@@ -13,7 +13,7 @@ require_once( dirname(__FILE__) . '/fp-api.php' );
 class BusinessPress extends BusinessPress_Plugin {
   
   
-  const VERSION = '0.7.1';
+  const VERSION = '0.7.2';
   
   
   private $disallowed_caps_default = array( 
