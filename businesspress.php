b3878081c1340111f5106c2644955985ec77b4cb
fvmartin <fvmartin@users.noreply.github.com>
2023-04-11 11:29:30 +0200

    User login sessions - Show on wp-admin user profile screen

--- businesspress.php
+++ businesspress.php
@@ -215,6 +215,14 @@ class BusinessPress extends BusinessPress_Plugin {
      */
     add_filter( 'recovery_mode_email', array($this , 'recovery_email') );
 
+    /**
+     * User login sessions
+     */
+
+    // Show on wp-admin user profile screen
+    add_action( 'show_user_profile', array( $this, 'user_session_tokens' ) );
+    add_action( 'edit_user_profile', array( $this, 'user_session_tokens' ) );
+
     parent::__construct();
     
   }
@@ -2050,6 +2058,57 @@ JSR;
     
   }
   
+  function user_session_tokens( $user ) {
+    if ( !current_user_can( 'edit_users', $user->ID ) ) {
+      return;
+    }
+
+    ?>
+    <h3 id="login-sessions"><?php esc_html_e( 'Login Sessions', 'businesspress' ); ?></h3>
+    <?php
+
+    $session_tokens = get_user_meta( $user->ID, 'session_tokens', true );
+
+    $active_tokens = array_filter( $session_tokens, function( $item ) {
+      return $item['expiration'] > time();
+    } );
+
+    $expired_tokens = array_filter( $session_tokens, function( $item ) {
+      return $item['expiration'] <= time();
+    } );
+
+    if ( $session_tokens ) : ?>
+      <p><span class="description"><?php printf( __( 'Found %d active and %d expired login sessions.', 'genesis' ), count( $active_tokens ), count( $expired_tokens ) ); ?></span></p>
+
+      <table class="widefat">
+        <thead>
+          <tr>
+            <td>Login Time</td>
+            <td>Expiration Time</td>
+            <td>IP Address</td>
+            <td>User Agent</td>
+          </tr>
+        </thead>
+        <?php foreach( array(
+          $active_tokens,
+          $expired_tokens
+        ) as $tokens ) : ?>
+          <?php foreach ( $tokens as $k => $v ) : ?>
+            <tr>
+              <td><abbr title="<?php echo date( 'r', $v['login'] ); ?>"><?php echo date( 'Y-m-d', $v['login'] ); ?></abbr></td>
+              <td style="background: <?php echo $v['expiration'] > time() ? '#bfb' : '#fbb'; ?>"><abbr title="<?php echo date( 'r', $v['expiration'] ); ?>"><?php echo date( 'Y-m-d', $v['expiration'] ); ?></abbr></td>
+              <td><?php echo $v['ip']; ?></td>
+              <td><?php echo $v['ua']; ?></td>
+            </tr>
+          <?php endforeach; ?>
+        <?php endforeach; ?>
+      </table>
+
+    <?php else : ?>
+      <p><span class="description"><?php esc_html_e( 'No login sessions found.', 'genesis' ); ?></span></p>
+    <?php endif;
+
+  }
   
   function wp_login_errors( $errors ) {
     if( isset($_GET['checkemail']) && $_GET['checkemail'] == 'confirm' && isset($errors->errors) && isset($errors->errors['confirm']) && isset($errors->errors['confirm'][0]) ) {
