2be1cb8e4ea44056c0a73879d0f796b8faf5c63f
Martin Vicenik <fvmartin@users.noreply.github.com>
2025-11-21 14:28:00 +0100

    IP Detect: Update Cloudflare IP ranges

--- businesspress.php
+++ businesspress.php
@@ -848,14 +848,40 @@ class BusinessPress extends BusinessPress_Plugin {
     $aProxies = array();
     
     // https://www.cloudflare.com/ips-v4
-    $aCloudFlareIP4 = array( '103.21.244.0/22','103.22.200.0/22','103.31.4.0/22','104.16.0.0/12','108.162.192.0/18','131.0.72.0/22','141.101.64.0/18','162.158.0.0/15','172.64.0.0/13','173.245.48.0/20','188.114.96.0/20','190.93.240.0/20','197.234.240.0/22','198.41.128.0/17','199.27.128.0/21' );
+    $aCloudFlareIP4 = "173.245.48.0/20
+      103.21.244.0/22
+      103.22.200.0/22
+      103.31.4.0/22
+      141.101.64.0/18
+      108.162.192.0/18
+      190.93.240.0/20
+      188.114.96.0/20
+      197.234.240.0/22
+      198.41.128.0/17
+      162.158.0.0/15
+      104.16.0.0/13
+      104.24.0.0/14
+      172.64.0.0/13
+      131.0.72.0/22";
+
+    $aCloudFlareIP4 = array_map( 'trim', explode( "\n", $aCloudFlareIP4 ) );
+
     $aProxies = array_merge( $aProxies, $aCloudFlareIP4 );
     
     // https://www.cloudflare.com/ips-v6
-    $aCloudFlareIP6 = array( '2400:cb00::/32', '2405:8100::/32', '2405:b500::/32', '2606:4700::/32', '2803:f800::/32', '2c0f:f248::/32', '2a06:98c0::/29' );
+    $aCloudFlareIP6 = "2400:cb00::/32
+      2606:4700::/32
+      2803:f800::/32
+      2405:b500::/32
+      2405:8100::/32
+      2a06:98c0::/29
+      2c0f:f248::/32";
+
+    $aCloudFlareIP6 = array_map( 'trim', explode( "\n", $aCloudFlareIP6 ) );
+
     $aProxies = array_merge( $aProxies, $aCloudFlareIP6 );
     
-    if (defined('WP_FAIL2BAN_PROXIES')) { //  todo: check this out      
+    if ( defined( 'WP_FAIL2BAN_PROXIES' ) ) { //  todo: check this out      
       $aProxies = array_merge( $aProxies, explode( ',' ,WP_FAIL2BAN_PROXIES ) );
     }
     
